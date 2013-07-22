6b7fc6ac851b88d2d61d27cdb4cdbab6a30dd1c7
Tom McFarlin <tom@tommcfarlin.com>
2013-07-22 08:43:57 -0400

    updating the responsible button to the inverse color

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <!--[if IE 8 ]><html id="ie8" <?php language_attributes(); ?>><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
-	<head>	
+	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
@@ -29,7 +29,7 @@
 					var _gaq = _gaq || [];
 					_gaq.push(['_setAccount', '<?php echo $global_options[ 'google_analytics' ] ?>']);
 					_gaq.push(['_trackPageview']);
-		
+
 					(function() {
 						var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 						ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
@@ -46,33 +46,33 @@
 			<?php get_template_part( 'page', 'offline-mode' ); ?>
 			<?php exit; ?>
 		<?php } // end if ?>
-		
+
 		<?php get_template_part( 'lib/breadcrumbs/standard_breadcrumbs' ); ?>
-		
+
 		<?php if( ! has_nav_menu( 'menu_below_logo' ) || has_nav_menu( 'menu_above_logo' ) ) { ?>
 			<div id="menu-above-header" class="menu-navigation navbar navbar-fixed-top">
-				<div class="navbar-inner ">
+				<div class="navbar-inner navbar-inverse">
 					<div class="container">
-		
+
 						<a class="btn btn-navbar" data-toggle="collapse" data-target=".above-header-nav-collapse">
 						  <span class="icon-bar"></span>
 						  <span class="icon-bar"></span>
 						  <span class="icon-bar"></span>
 						</a>
-										
-						<?php if( is_front_page() || 'video' == get_post_format() || 'image' == get_post_format() || '' == get_the_title() ) { ?>					
+
+						<?php if( is_front_page() || 'video' == get_post_format() || 'image' == get_post_format() || '' == get_the_title() ) { ?>
 							<h1 id="site-title">
 								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php get_bloginfo( 'name' ); ?>" rel="home" class="brand"><?php bloginfo( 'name' ); ?></a>
 							</h1>
 						<?php } else { ?>
 							<p id="site-title">
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php get_bloginfo( 'name' ); ?>" rel="home" class="brand"><?php bloginfo( 'name' ); ?></a>	
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php get_bloginfo( 'name' ); ?>" rel="home" class="brand"><?php bloginfo( 'name' ); ?></a>
 							</p>
 						<?php } // end if/else ?>
-									
-						<div class="nav-collapse above-header-nav-collapse">													
+
+						<div class="nav-collapse above-header-nav-collapse">
 							<?php
-								wp_nav_menu( 
+								wp_nav_menu(
 									array(
 										'container_class'	=> 'menu-header-container',
 										'theme_location'  	=> 'menu_above_logo',
@@ -83,17 +83,16 @@
 								 );
 							?>
 
-						</div><!-- /.nav-collapse -->		
-						
+						</div><!-- /.nav-collapse -->
+
 						<?php $social_options = get_option( 'standard_theme_social_options' ); ?>
 						<?php if( '' != $social_options['active-social-icons'] ) { ?>
 							<div id="social-networking" class="clearfix">
-								<?php get_template_part( 'social-networking' ); ?>  
-							</div><!-- /#social-networking -->	
+								<?php get_template_part( 'social-networking' ); ?>
+							</div><!-- /#social-networking -->
 						<?php } // end if ?>
 
 					</div> <!-- /container -->
 				</div><!-- /navbar-inner -->
-			</div> <!-- /#menu-above-header -->	
+			</div> <!-- /#menu-above-header -->
 		<?php } // end if ?>
-		
